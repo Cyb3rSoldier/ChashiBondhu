6803777ca99a8f6123cce70934db72d9fbb0e0d4
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2026-08-17 00:51:34 +0600

    config updated

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";  /* sql206.infinityfree.com */
-$user = "root";  /* if0_41922519 */
-$password = "";  /* chashibondhu123 */
-$database = "chashibondhu";  /* if0_41922519_chashibondhu */
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "chashibondhu";
 
 $conn = new mysqli($host, $user, $password, $database);
 
